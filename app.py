60820184cc746bc40c132e716a5846b782685400
Darcyos <darcyosoares2017@gmail.com>
2023-06-25 21:13:21 -0300

    primeiro commit

--- app.py
+++ app.py
@@ -2,16 +2,16 @@ from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen, ScreenManager
 import tkinter.messagebox as MessageBox
-
+import re
 import mysql.connector
 
-'''conexao = mysql.connector.connect(
+conexao = mysql.connector.connect(
     host= 'localhost',
     user= 'root',
     password= '',
-    database= 'escola_ete'
+    database= 'point_farma'
 )
-cursor = conexao.cursor()'''
+cursor = conexao.cursor()
 
 class GerenciarLogin(ScreenManager):
     pass
@@ -47,12 +47,22 @@ class Login(Screen):
     def entrar(self):
         usuario = self.ids.usuario.text
         senha = self.ids.senha.text
-        if usuario =='darcyo' and senha=="123456":
-            return GerenciarLogado
+        comando = "SELECT * FROM cliente where email = %s and senha = %s"
+        valores = (usuario, senha)
+        cursor.execute(comando, valores)
+        
+        if cursor.fetchone() is not None:
+            self.successful_login()
         else:
-            MessageBox.showinfo("ERRO!", "usuario ou senha invalidos!")
+            print("Falha na autenticação. Usuário ou senha inválidos.")
 
 
+         
+    
+
+
+    def successful_login(self):
+        self.manager.current = 'logado'
 
 class App(MDApp):
     def build(self):
@@ -63,5 +73,28 @@ class App(MDApp):
     
 
 App().run()
-'''cursor.close()
-conexao.close()'''
+cursor.close()
+conexao.close()
+
+
+
+
+ #identificar se os dados inseridos é de um email 
+'''if re.match(r"[^@]+@[^@]+\.[^@]+", usuario):
+            tipo = 'email'
+    
+        if tipo=='email':
+            comando = f"SELECT * FROM cliente where email = '{usuario}'"
+            cursor.execute(comando)
+            dados = cursor.fetchall() 
+
+                #self.successful_login()
+
+            #identificar se os dados inseridos é de um celular
+        elif re.match(r"\d{2}\s?\d{4,5}\s?\d{4}", usuario):
+            comandouser = f"SELECT numero FROM cliente where numero = '{usuario}'"
+            cursor.execute(comandouser)
+            user = cursor.fetchall()
+            comandosenha = f"SELECT senha FROM cliente where email = '{usuario}'"
+            cursor.execute(comandosenha)
+            password = cursor.fetchall()'''
